a8eff868b1f01bab74bc0ce13e72310522887a73
Niedja Araujo <naraujos1007@hotmail.com>
2025-03-04 19:21:17 -0300

    convertendo para real e centavos

--- script.js
+++ script.js
@@ -2,5 +2,17 @@ const amount = document.getElementById("amount");
 
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, ""); //recebendo o input e tirando as letras
-  amount.value = value; // recebendo so numeros
+
+  //convertendo em numero e transformar em centavos
+  value = Number(value) / 100;
+  amount.value = formatCurrencyBRL(value); // recebendo so numeros
 };
+
+// formata o valor para moeda Real
+function formatCurrencyBRL(value) {
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+  return value;
+}
